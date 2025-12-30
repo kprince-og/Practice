65de46ba9e6ea28b8b8667e3255e9b84e7d4a9b6
kprince-og <kprince7423@gmail.com>
2025-12-30 21:08:47 +0530

    add files

---
+++ js practice/pract.js
@@ -0,0 +1,116 @@
+// -----------------Practise set 1
+
+// let num = prompt("Enter the Number");
+
+// if (num % 5 === 0 ){
+//     console.log(num ,"is Multiple of 5");
+// }
+// else {
+//    console.log(num ,"is not Multiple of 5");
+// }
+
+// -----------------Practise set 2
+
+// let score = prompt("Enter your Score (0-100)");
+// let grade;
+
+// if (score >= 90 && score <= 100){
+//     grade = "A";
+// }
+// else if (score >= 70 && score <=89){
+//     grade = "B";
+// }
+// else if (score >= 60 && score <= 69){
+//     grade = "C";
+// }
+// else if (score >= 50 && score <= 59){
+//     grade = "D";
+// }
+// else if (score > 0 && score <= 49){
+//     grade = "F";
+// }
+// else {
+//     grade = "Unidentified Score";
+// }
+
+// console.log("Your Grade is :",grade);
+
+//----------looping statement practice Q1.(print even numbers from 0-100)
+
+// for (i=0; i<=100; i++){
+//     if(i % 2 === 0){
+//         console.log("even number :",i);
+//     }
+// }
+
+//----------looping statement practice Q2.(guessing number game)
+
+// let gameNum = 75;
+// let userNum = prompt("Guess the game number :");
+
+// while(userNum != gameNum){
+//     userNum = prompt("You Guessed the Wrong number. Guess Again!");
+// }
+
+// console.log(userNum);
+// console.log("Congrats! you Got the correct answer");
+
+// ---------------------------------------------Practice for Strings in JS Q.no1
+
+// let fullName = prompt("Enter your Full Name Here...! without any spacing");
+
+// let username = "@" + fullName + fullName.length;
+
+// console.log("Username :",username);
+// console.log("Name :",fullName);
+
+// ----------------------------Practice for Arrays Q.no 1 avg marks of entire class
+
+// let marks = [34,92,56,68,95,76];
+
+// let sum = 0;
+
+// for (let val of marks){
+//     sum += val;
+// }
+
+// let avg = sum / marks.length;
+
+// console.log(`Total marks of Entire Class = ${sum}`);
+// console.log(`Average Marks of Entire Class = ${avg}`);
+
+// ----------------------------Practice for Arrays Q.no 2 price before offer
+
+// let items = [250,645,300,900,50];
+
+//----------------------by for-of loop
+
+// let i = 0;
+// for (let val of items){
+//     console.log(`values : ${val}`);
+//     let offer = val / 10;
+//     items[i] = items[i] - offer;
+//     console.log(`final price : ${items[i]}`);
+//     i++;
+// }
+
+//----------------------by for loop
+
+// for(let i = 0; i < items.length; i++){
+//     let offer = items[i] / 10;
+//     items[i] -= offer;
+// }
+
+// console.log(items);
+
+// ----------------------------practice for array method Q.no 1
+
+// let companies = ["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+
+// companies.shift();// remove first company from an array
+
+// companies.splice(1 , 1 , "Ola");// remove uber and place ola in its place
+
+// companies.push("Amazon");// add amazon to the end of an array
+
+// console.log(companies);
---
+++ js practice/script.js
@@ -0,0 +1,377 @@
+//--------------------------------------JS Variables and Data types
+
+
+// -----------------------Data types in JS
+
+// let Name = "Tony stark";
+// let age = 34;
+// let totalPrice = 599;
+
+// let price = 109.87;
+// let fullName = "Shaktiman";
+// isFollow = true;
+// let b;
+// let a = null;
+// let x = BigInt("123");
+// let y = Symbol("Hello JS !");
+
+// ---------------------------output prints used commands
+
+// console.log (Name);
+// console.log (age);
+// console.log (totalPrice);
+
+// ---------------------------object creation
+
+// const student = {
+//     name : "Salini",
+//     age : 20,
+//     cgpa : 7.8,
+//     isPass : true
+// };
+
+// student ["name"] = "Sourav Verma";
+// student ["age"] = student ["age"] + 1;
+
+// console.log(student.name);
+
+// ----------------------------------Practice For variables and Data types in JS 1
+
+// const product = {
+//     title : "Trimax Fountain Gel Pen",
+//     ratings : 4.5,
+//     isDeal : true,
+//     price : 239,
+//     offer : 11
+// };
+
+// console.log(product);
+
+// // --------------------------------Practice For variables and Data types in JS 2
+
+// const profile = {
+//     username : "@Gamerbuddy",
+//     isFollow : true,
+//     name : "Gamer Buddy",
+//     posts : 19,
+//     followers : 278,
+//     following : 18,
+//     bio : "Content creator and a Youtuber"
+// };
+
+// console.log(profile);
+
+//-------------------------------------- 2nd Chapter of JS Variables and Data types
+
+// Arithmetic operators
+
+// let a = 9;
+// let b = 3;
+
+// console.log("a =", a , "& b =", b);
+// console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+// console.log("a % b =", a % b);//Modulus
+// console.log("a ** b =", a ** b);//Exponantiation
+
+
+// -------------------------------------Unary operators
+
+// let x = 10;
+// let y = 2;
+
+// console.log("x =", x , "& y =", y);
+// x++;
+// y--;
+
+// console.log("x+1 =", x, "& y-1 =", y);
+
+// ------------------------------------Assignment operators
+
+// let x = 10;
+// let y = 2;
+
+// x *= 3;
+
+// console.log("x =", x);
+
+// ------------------------------------comparison opertors
+
+// let x = 10;
+// let y = 2;
+
+// console.log("x =", x);
+// console.log("y =", y);
+
+// console.log("x != y", x != y);
+
+// --------------------------------------logical operators (AND , OR )
+
+// let x = 10;
+// let y = 2;
+
+// let cond1 = x > y ;
+// let cond2 = x === y ;
+
+// console.log("cond1 || cond2 =", cond1 || cond2);
+
+// -----------------Logical NOT operator
+
+// let x = 10;
+// let y = 2;
+
+// let cond1 = x > y ;
+// let cond2 = x === y ;
+
+// console.log("!(x < y)", !(x < y));
+
+// ---------------------------------Conditional Statements (IF and IF-ELSE)--------------------
+
+// let age = 24 ;
+
+// if (age >= 18 )
+// {
+//     console.log("You are eligible for Voting");
+// }
+
+// if (age < 18 )
+// {
+//     console.log("You are not eligible for Voting");
+// }
+
+// ---------------IF-ELSE
+
+// let mode = "dark" ;
+// let color;
+
+// if (mode === "dark")
+// {
+//     color = "black";
+// }
+
+// else 
+// {
+//     color = "white";
+// }
+
+// console.log(color);
+
+// --------------------find the odd or even number practice for if-else
+    
+// let a = 18;
+
+// if (a % 2 === 0){
+//     console.log(a ,"is even number");
+// } else {
+//     console.log(a ,"is odd number");
+// }
+
+// --------------else if statements
+
+// let age = 38;
+
+// if (age < 18){
+//     console.log("Teenagers");
+// } else if (age > 60 ){
+//     console.log("Seniors");
+// } else {
+//     console.log("Youngsters");
+// }
+
+// ----------ternary operator
+
+// let age = 19;
+
+// let result = age >= 18 ? "adult" : "not adult";
+// console.log(result);
+
+//--------------------------looping statements---------------------------
+
+// ---------------print 1 to 5 values(FOR Loop)
+// for ( i=1; i<=5; i++) {
+//     console.log("Hello JS !");
+// }
+
+// calculate the sum of 1 to 5 numbers
+
+// let sum = 0;
+
+// for(i = 1; i<=5; i++){
+//     sum = sum+i;
+// }
+// console.log("sum =", sum);
+
+// ------------------------While Loop
+
+// let i = 1;
+
+// while (i<=6){
+//     console.log("i =", i);
+//     i++;
+// }
+
+// ------------------------Do-While Loop
+
+// let i = 1;
+
+// do {
+//     console.log("Web Dev");
+//     i++;
+// } while (i<=10);
+
+// ----------for-of loop
+
+// let str = "Spiderman";
+// let size = 0;
+
+// for(let val of str) {
+//     console.log("value =",val);
+//     size++;
+// }
+
+// console.log("size of string =",size);
+
+// ----------for-in loop
+
+// let student = {
+//     name: "Sourav",
+//     branch: "cse",
+//     rollno: 192,
+//     cgpa: 6.8,
+//     isPass: true
+// };
+
+// for (let key in student) {
+//     console.log("key =",key , "value =", student[key]);
+// }
+
+// --------------------------------------strings in js
+
+// let str = "Monday";
+
+// console.log(str.length);
+// console.log(str[3]);
+
+// --------------------------------template literals
+
+// let obj = {
+//     item: "Book",
+//     price: 250
+// };
+
+// let output =`The Cost of ${obj.item} is ${obj.price} rupees`;
+
+// console.log(output);
+
+// -----------------------------------string methods (uppercase and lowercase)
+
+// let str = "WeekEnd";
+
+// let str1= str.toUpperCase();
+// let str2= str.toLowerCase();
+// console.log(str);
+// console.log(str1);
+// console.log(str2);
+
+// --------trim method
+
+// let str = "       Weekend begins from Today            ";
+
+// console.log(str.trim());
+
+// --------slice method(spits string from start to end)
+
+// let str = "Hello JS";
+
+// console.log(str.slice(1,3));
+
+// --------concat method(joins one string to another)
+
+// let str = "Web Dev ";
+// let str1 = "Code in JS"
+
+// let res = str + str1;// simplified way
+
+// console.log(str.concat(str1));// method for concatination
+// console.log(res);
+
+// --------replace method
+
+// let str = "Hello";
+
+// console.log(str.replace("lo","p"));
+
+// --------char at find method
+
+// let str = "Code of JS";
+
+// console.log(str.charAt(0));
+
+// ---------------------------------------Arrays in JS
+
+// let Subject = ["English","Maths","Science","SST","Hindi","Punjabi"];
+
+//----------------for loop
+// for (i= 0 ; i< Subject.length ; i++){
+//     console.log(Subject[i]);
+// }
+
+//-------for-of loop
+
+// let cities = ["Amritsar","Gurdaspur","Pathankot","Tarn Taran"];
+
+// for(let city of cities){
+//     console.log(city);
+// }
+
+// -----------------------Array Methods in JS (push , pop & toString() Method)
+
+// let foodItems = ["Paneer tikka","cigar roll","Mashroom duplex","burger"];
+
+//----------------push method (used for adding something in end position)
+// foodItems.push("Apple","Mango","Guava");
+
+//----------------pop method (used to delete something in the end position)
+// foodItems.pop();
+
+// console.log(foodItems);
+// console.log(foodItems.toString()); //------------converts array into string without changing the actual array.
+
+// ----------------------------more array methods (concat(), unshift(), shift())
+
+// let marvelHeroes = ["iron_man","hulk","captain_america","thor"];
+// let dcHeroes = ["superman","batman","flash","deadpool"];
+
+// marvelHeroes.unshift("Dr.Strange");// add something to starting position
+// dcHeroes.shift();// delete something from starting position
+
+// let heroes = marvelHeroes.concat(dcHeroes);// multiplies 2 array with other in an new array
+
+// console.log(heroes);
+
+// --------------------slice method
+
+// let marvelHeroes = ["iron_man","hulk","captain_america","thor","spiderman"];
+
+// console.log(marvelHeroes.slice(1));//------ returns a piece of an array
+
+// --------------------------splice mathod
+
+// let marks = [24,34,57,93,86,67,75];
+
+// marks.splice(2, 4, 45, 47, 39 ,28);// used to add , delete and replace the number in an array.
+
+// console.log(marks);
+
+// ------------------------------------Functions and methods in JS
+
+//------------functions
+
+function myfunction() { // function defination
+    console.log("Welcome Developers !");
+    console.log("We are Learning JS :)");
+}
+
+myfunction();// function call
